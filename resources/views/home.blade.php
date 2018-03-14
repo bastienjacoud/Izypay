85ca7e034997d152ee08c17a95f17138a63ce0f6
BastienJacoud <bastien.jacoud@etu.univ-lyon1.fr>
2018-03-14 13:53:24 +0100

    Commit initial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.master')
+@section('content')
+<div class="container">
+    <h1 class="bvn"> Gestion des oeuvres et de leurs réservations </h1>
+</div>
+@stop
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" id="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header col-xs-8 offset-xs-6">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="{{ route('insertionBD') }}" data-toggle="collapse" data-target=".navbar-collapse.in">Insertion</a></li>
+                    <li><a href="{{ route('afficheTransaction') }}" data-toggle="collapse" data-target=".navbar-collapse.in">Affichage</a></li>
+                    <li><a href="{{ route('bonus') }}"data-toggle="collapse" data-target=".navbar-collapse.in">Bonus</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    @yield('content')
+</div>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
